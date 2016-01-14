15ada323f97f107b7fdb691bb8a05d5a8733a7ed
sergioarc <sergioar@ciencias.unam.mx>
2016-01-13 22:47:08 -0600

    Agregado app.js y appRoues.js

---
+++ public/angular/appRoutes.js
@@ -0,0 +1,21 @@
+/*Administración de las rutas para mostrar y controlar en el front-end */
+
+angular.module('viewsRoutes', []).config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider){
+	
+	$routeProvider
+		// HOME
+		.when('/', {
+			templateUrl: 'public/angular/views/pages/inicio.html'
+		})
+
+		// RUTAS INVÁLIDAS (no descritas previamente)
+		.otherwise({
+			redirectTo: '/'
+		});
+	
+
+	// establecer el uso de URLS modernas (sin #)
+	$locationProvider.html5Mode(true);
+}]);
+
+
