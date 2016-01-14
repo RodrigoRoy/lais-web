9d65f53253e1fcb0ea1e552c5b4e0b6ae06c260c
sergioarc <sergioar@ciencias.unam.mx>
2016-01-14 12:44:10 -0600

    Se agrego una seccion del inicio y el carrusel

---
+++ public/imgs/LAIS-Web_files/app.js
@@ -0,0 +1,6 @@
+/*Se define la aplicación de angular cargando sus principales modulos necesrios para la aplicación como
+dependencias, servicios(factory) y controladores*/
+
+
+angular.module("laisApp",["ngRoute","ngMessages","ngCookies","ngAnimate","ngSanitize","viewsRoutes"]);
+
