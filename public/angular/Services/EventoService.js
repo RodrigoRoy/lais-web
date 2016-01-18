e6ce1b6665b85671bffd339927b280bb732c77ee
sergioarc <sergioar@ciencias.unam.mx>
2016-01-18 16:56:38 -0600

    Agregue la parte del servicio de eventos y de diseño un icono de usuario

---
+++ public/angular/Services/EventoService.js
@@ -0,0 +1,33 @@
+/*Servicio(Factory) que administra las llamadas a servidor para obtener,editar,enviar y eliminar datos de los eventos a la base de datos*/
+
+
+
+angular.module('EventoService', []).factory('Evento', ['$http', function($http){
+	return {
+		// Obtiene todos los eventos
+		all: function(){
+			return $http.get('/api/eventos');
+		},
+
+		// Obtiene el evento en particular con el eventoID
+		get: function(eventoID){
+			return $http.get('/api/eventos/' + eventoID);
+		},
+
+		//Crea un nuevo evento, con el lo que le enviemos de datos(eventoData)
+		create: function(eventoData){
+			return $http.post('/api/eventos', eventoData);
+		},
+
+		// Actualiza la información de un evento (mediante el eventoID y la información nueva eventoID)
+		// 
+		update: function(eventoID, eventoData){
+			return $http.put('/api/eventos/' + eventoID, eventoData);
+		},
+
+		//Elimina el evento con su respectivo eventoID
+		delete: function(eventoID){
+			return $http.delete('/api/eventos/' + eventoID);
+		}
+	}
+}]);
