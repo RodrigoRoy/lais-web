2e7c10ec47006d9d3e1b7542e0cc3c37c862f879
sergioarc <sergioar@ciencias.unam.mx>
2016-01-19 14:16:49 -0600

    unos cambios

--- public/angular/app.js
+++ public/angular/app.js
@@ -1,5 +1,3 @@
-/*Se define la aplicación de angular cargando sus principales modulos necesrios para la aplicación como
-dependencias, servicios(factory) y controladores*/
 
 
 angular.module("laisApp",["ngRoute","ngMessages","ngCookies","ngAnimate","ngSanitize","ui.bootstrap","viewsRoutes"]);
