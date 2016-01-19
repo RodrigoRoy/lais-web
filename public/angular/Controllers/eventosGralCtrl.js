5ff7d7df3d6945ec0ab9a5068809631a44a214e0
sergioarc <sergioar@ciencias.unam.mx>
2016-01-19 11:30:37 -0600

    nuevos eventos

---
+++ public/angular/Controllers/eventosGralCtrl.js
@@ -0,0 +1,13 @@
+/*Controlador de la página de eventos, se encarga de obtener todos los eventos existentes de la base de datos*/
+
+angular.module('EventosCtrl', []).controller('EventosController', function($scope, $location, $cookies, Evento){
+	$scope.eventos = [];
+	
+	Evento.all() //Se trae todos los eventos usando el servicio de Evento
+		.then(function(res){ 
+			$scope.eventos = res.data;
+			console.log($scope.eventos);
+		}, function(res){ 
+			alert('Error de Conexión con la Base de Datos');
+		});
+});
