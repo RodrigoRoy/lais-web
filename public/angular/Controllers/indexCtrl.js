600df51eb3dc19487ae3ea4c0ef69c4ae898a861
sergioarc <sergioar@ciencias.unam.mx>
2016-01-25 12:59:03 -0600

    modificaciones del inicio

--- public/angular/Controllers/indexCtrl.js
+++ public/angular/Controllers/indexCtrl.js
@@ -4,11 +4,33 @@
 	y para que funcione "href" en páginas distintas a la de inicio (por ejemplo: /eventos).
 */
 
-angular.module('IndexCtrl',[]).controller('IndexController', function ($scope, $location){
+angular.module('IndexCtrl',[]).controller('IndexController', function ($scope, $location,  $uibModal, $log){
 	$scope.$on('$locationChangeStart', function(event) { // Se ejecuta en cada cambio de URL
 	    if($location.path() === '/') // Verificar si estamos en la página inicial
 	    	$scope.isIndex = true; // Variable que se utiliza en la vista (index.html)
 	    else
 	    	$scope.isIndex = false;
 	});
+
+
+	$scope.items = ['item1', 'item2', 'item3'];
+
+  	$scope.animationsEnabled = true;
+
+	$scope.open = function (size) {
+		var modalInstance = $uibModal.open({
+			animation: $scope.animationsEnabled,
+		  	templateUrl: 'iniciasSesionModalContent.html',
+		  	size: size,
+		  	resolve: {
+		    	items: function () {
+		      		return $scope.items;
+		    	}
+		  	}
+		});
+	};
+
+	$scope.iniciarSesion = function (usuario, pass){
+		
+	}
 });
