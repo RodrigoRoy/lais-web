7541a4bc318a156ddafe888612c6b869b2204e0d
Rodrigo Colín <rodrigo.cln@ciencias.unam.mx>
2016-08-24 18:03:47 -0500

    página individual para evento actualizada con Google Maps y archivos adjuntos

--- public/angular/Controllers/eventoCtrl.js
+++ public/angular/Controllers/eventoCtrl.js
@@ -1,38 +1,45 @@
 /*Controlador que manda a llamar la información de un evento con su id */
 
-angular.module('EventoCtrl',[]).controller('EventoController', function ($scope, $location, $routeParams, $cookies, Evento){
+angular.module('EventoCtrl',[]).controller('EventoController', function ($scope, $routeParams, Evento){
+
+	// Carga síncrona (no recomendada) para mostrar correctamente el mapa en pantalla
+	// var initialize = function(){
+	// 	var map = new google.maps.Map(document.getElementById('map'), {
+	// 		center: {lat: 19.356751, lng: -99.166728},
+	// 		zoom: 15
+	// 	});
+	// };
+	// google.maps.event.addDomListenerOnce(window, 'load', initialize);
 
 	//Obtiene el evento con su ID
 	Evento.get($routeParams.id)
 		.then(function(res){
-			$scope.titulo = res.data.titulo;
-			$scope.descripcion = res.data.descripcion;
-			$scope.imagenPrincipal = res.data.imagenPrincipal;
-			$scope.contenidoHTML = res.data.contenidoHTML;
-			$scope.tipo = res.data.tipo;
-			$scope.lugar = res.data.lugar.direccion;
-
-
-			var direccion = []
-			for(key in $scope.lugar){
-				if(key == "numero")
-					direccion.push("#"+$scope.lugar[key])
-            	else if(key == "cp")
-            		direccion.push("C.P. "+$scope.lugar[key])
-            	else if(key == "colonia")
-            		direccion.push("Col. "+$scope.lugar[key])
-            	else 
-            		direccion.push($scope.lugar[key]);
-        	}
-        	console.log("Direc: ", $scope.lugar)
-			$scope.direccion = direccion.join(", ");
+			$scope.evento = res.data;
 
 			//Join para guardar a los realizadores en una cadena
-			$scope.realizador = res.data.realizador.join(", ");
-			
-			//Join para guardar los telefonos en una cadena
-			$scope.tels = res.data.lugar.telefono.join(", ");
-			//console.log($scope.tels);
-			
-		})	
+			$scope.realizador = $scope.evento.realizador.join(", ");
+
+			setTimeout(function(){
+				var map = new google.maps.Map(document.getElementById('map'), {
+					center: {lat: 19.356751, lng: -99.166728},
+					zoom: 15
+				});
+				// console.log("map", map);
+			}, 500);
+
+			// Reverse Geocoding para obtener dirección a partir de PlaceID ($scope.evento.lugar)
+			var geocoder = new google.maps.Geocoder;
+			geocoder.geocode({placeId: $scope.evento.lugar}, function(res, status){
+				if(status == google.maps.GeocoderStatus.OK){
+					$scope.$apply(function(){ // Actualiza binding en llamadas asíncronas
+						$scope.direccion = res[0].formatted_address;
+					});
+				}
+			});
+
+			// TODO: Agregar marcador al mapa en base a respuesta de Geocoder
+			// var marker = google.maps.Marker({
+			// 	map: map
+			// });
+		});
 })
