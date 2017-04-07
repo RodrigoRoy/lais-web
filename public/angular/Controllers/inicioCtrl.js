21da487d3b2d624f6b0cf137ddb5a127b793530e
Rodrigo Colín <rodrigo.cln@ciencias.unam.mx>
2017-04-07 14:31:23 -0500

    varios cámbios estéticos, de contenido y CSS

--- public/angular/Controllers/inicioCtrl.js
+++ public/angular/Controllers/inicioCtrl.js
@@ -1,15 +1,15 @@
 //Controlador que maneja la página de Inicio
 
-angular.module('InicioCtrl',[]).controller('InicioController', function ($http, $scope, Evento) {
+angular.module('InicioCtrl',[]).controller('InicioController', function ($scope) { // Evento
 	
 	$scope.myInterval = 5000; //Intervalo de tiempo para pasar de un slide a otro 
+	$scope.slides = ["city-q-g-900-500-8.jpg", "nature-q-c-900-500-3.jpg", "people-q-c-900-500-7.jpg", "transport-q-c-900-500-9.jpg"]; // Nombres de imagenes en public/imgs/carrusel
 
-	Evento.news() //Se trae los 4 eventos para el slide
-		.then(function(res){ 
-			$scope.slides = res.data;
-			//console.log($scope.slides);
-		}, function(res){ 
-			alert('Error de Conexión con la Base de Datos');
-		});
+	// Evento.news() //Se trae los 4 eventos para el slide
+	// 	.then(function(res){ 
+	// 		$scope.slides = res.data;
+	// 	}, function(res){ 
+	// 		alert('Error de Conexión con la Base de Datos');
+	// 	});
 
 })
