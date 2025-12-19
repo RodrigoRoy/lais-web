03d2aab6a2706db861ebdee686bf05f94a3f5c1e
Rodrigo Colín <rodrigo.cln@ciencias.unam.mx>
2025-12-19 11:35:21 -0600

    catálogo mujeres documentalistas agregado

--- public/angular/Controllers/sitiosCtrl.js
+++ public/angular/Controllers/sitiosCtrl.js
@@ -6,31 +6,37 @@ angular.module('SitiosCtrl', []).controller('SitiosController', function ($scope
 			title: 'Huellas de Luz',
 			text: 'Da acceso a imágenes de América Latina, siglos XIX y XX. Incluye documentación que permite ir más allá de las imágenes y conocer acerca de sus contextos de producción. Vincula con archivos de imágenes de diversos países latinoamericanos y quienes las estudian.',
 			url: 'https://lais.mora.edu.mx/huellasdeluz',
-			img: 'Huellas_de_Luz.png',
+			img: 'Huellas_de_Luz.jpg',
 		},
 		{
 			title: 'MetaDOC',
 			text: 'Sitio web que pone a disposición la catalogación de la colección de documentales del Laboratorio Audiovisual de Investigación Social del Instituto Mora, con la intención de impulsar el análisis e investigación en diversidad de producciones de cine documental.',
 			url: 'http://lais.mora.edu.mx/metadoc',
-			img: 'metaDOC.png',
+			img: 'metaDOC.jpg',
+		},
+		{
+			title: 'Catálogo Mujeres Documentalistas en México 1970-1985',
+			text: 'Reúne a realizadoras, camarógrafas, sonidistas, editoras, guionistas, fotógrafas, productoras, animadoras y preservadoras que participaron en el quehacer documental entre 1970 y 1985.',
+			url: 'https://lais.mora.edu.mx/mujeresdocumentalistas/',
+			img: 'Catálogo_Mujeres_Documentalistas.jpg',
 		},
 		{
 			title: 'Mapeo de Laboratorios Audiovisuales en México',
 			text: 'Mapeo de espacios colectivos cuyo eje de trabajo es la investigación sobre lo audiovisual y con herramientas audiovisuales, ofreciendo una ponencia en video, imágenes, fotografías, contacto, vínculos a sus páginas y redes sociales.',
 			url: 'https://lais.mora.edu.mx/laboratorios',
-			img: 'Mapeo_laboratorios.png',
+			img: 'Mapeo_laboratorios.jpg',
 		},
 		{
 			title: 'Diálogos documentales de Isla Tortuga al Abya Yala',
 			text: 'Ciclo de charlas y proyecciones sobre la lucha y resistencia de los pueblos originarios en torno a problemáticas como la conservación del territorio, revitalización de la lengua y la participación de las mujeres.',
 			url: 'https://lais.mora.edu.mx/proyecciones',
-			img: 'Abya_Yala.png',
+			img: 'Abya_Yala.jpg',
 		},
 		{
 			title: '¿Cómo la ves? Exposición interactiva',
 			text: 'Interactivo diseñado para los padres, tutores y maestros que estamos interesados en reflexionar sobre el consumo indiscriminado y excesivo de la televisión comercial por parte de nuestros hijos y alumnos.',
 			url: 'https://www.institutomora.edu.mx/Investigacion/Interactivo_como_la_ves/index.htm',
-			img: 'Como_la_ves.png',
+			img: 'Como_la_ves.jpg',
 		},
 	];
 });
